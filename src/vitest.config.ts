aa28df853cebc5b0081661bc40f6864ee64c3748
lihs-ie <lihs.dev@gmail.com>
2025-02-14 12:43:16 +0900

    sam setup

---
+++ src/vitest.config.ts
@@ -0,0 +1,30 @@
+import path from "path";
+import { defineConfig } from "vitest/config";
+
+/// <reference types="vitest" />
+export default defineConfig({
+  test: {
+    environment: "node",
+    globals: true,
+    alias: {
+      "^acl/(.*)$": "/src/acl/$1",
+      "^aspects/(.*)$": "/src/aspects/$1",
+      "^config(.*)$": "/src/config/$1",
+      "^domains(.*)$": "/src/domains/$1",
+      "^extensions(.*)$": "/src/extensions/$1",
+      "^infrastructures(.*)$": "/src/infrastructures/$1",
+      "^tests(.*)$": "/src/tests/$1",
+      "^components(.*)$": "/src/components/$1",
+    },
+    setupFiles: ["./vitest.setup.ts"],
+  },
+  esbuild: {
+    tsconfigRaw: require("./tsconfig.json"),
+  },
+  resolve: {
+    alias: {
+      "@root": path.resolve(__dirname),
+      "@tests": path.resolve(__dirname, "tests"),
+    },
+  },
+});
